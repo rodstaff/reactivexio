e4596017461a0b75e04dc8536baf33ccac032448
rodk <rod2010rod2010@hotmail.com>
2016-07-01 19:14:44 -0400

    added map2.js

---
+++ map2.js
@@ -0,0 +1,5 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+var numbers = [1, 4, 9];
+var roots = numbers.map(Math.sqrt);
+// roots is now [1, 2, 3], numbers is still [1, 4, 9]
+console.log(numbers, roots);

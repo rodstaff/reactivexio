6cc0e40a448898a5307c54be479fa3faba2e0a26
rodk <rod2010rod2010@hotmail.com>
2016-07-03 22:24:41 -0400

    added aDataFiles

---
+++ aDataFiles/areDramas.js
@@ -0,0 +1,22 @@
+		module.exports = {
+			
+			name: "Dramas",
+			videos: [
+				{
+					"id": 65432445,
+					"title": "The Chamber",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 675465,
+					"title": "Fracture",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id:432534, time:65876586 }]
+				}
+			]
+		}
---
+++ aDataFiles/freshReleases.js
@@ -0,0 +1,22 @@
+	    module.exports = {
+	    	
+			name: "Fresh Releases",
+			videos: [
+				{
+					"id": 70111470,
+					"title": "Die Hard",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 654356453,
+					"title": "Bad Boys",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id:432534, time:65876586 }]
+				}
+			]
+		}
